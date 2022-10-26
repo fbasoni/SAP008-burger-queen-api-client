4d78d5f0c155e042fc837338b2bd3a0439f0c717
fabi basoni <basoni.fabiany@gmail.com>
2022-10-26 16:34:29 -0300

    iniciando homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import './App.css'
+import logo from './Assets/Crown.gif'
+import signUpButton from "Buttons";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Burger Queen</h1>
-    </div>
+      <div className="App">
+        <div>
+          <img src={logo} alt="logo"></img>
+        </div>
+        <section>
+          <signUpButton />
+          <loginButton />
+        </section>
+      </div>
   );
 }
 
---
+++ src/Buttons.js
@@ -0,0 +1,16 @@
+export const signUpButton = () => {
+  return (
+  <div>
+    <button className="signup-button">Sign up</button>
+  </div>
+  )
+}
+
+export const loginButton = () => {
+  return (
+  <div>
+    <button className="login-button">Login</button>
+  </div>
+  )
+}
+
