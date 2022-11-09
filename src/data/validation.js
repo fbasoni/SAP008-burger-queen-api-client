2d13c491e1c0360e41f8f3c7fe5764b4c6953719
fabi basoni <basoni.fabiany@gmail.com>
2022-11-09 11:34:53 -0300

    apagando linhas extras

--- src/data/validation.js
+++ src/data/validation.js
@@ -11,12 +11,10 @@ export function handleRegisterErrors (errorCode) {
 
 export function handleLoginError(errorCode){
     if (errorCode === 400) return "Invalid e-mail or password.";
-    
 }
 
 export function validateEmail(email) {
     const emailRegex = /[a-z0-9._%+-]+@[a-z]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
-
     const validation = emailRegex.test(email);
 
     if (!email) return "Please, enter your e-mail."
@@ -28,7 +26,6 @@ export function validateName(name) {
 }
 
 export function validatePassword(password) {
-
     if (!password) return "Please, enter your password."
     if (password.length < 6) return "Your password must be at least 6 characters long."
 }
